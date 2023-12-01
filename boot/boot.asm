52e3a9093fb62ae6301bdfb1eaef4668d6f5608e
Dexter Harrison <dexterharrison229@gmail.com>
2023-11-30 20:58:25 -0500

    bootloader test;

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,10 +1,23 @@
 ORG 0x7c00
 BITS 16
 start:
-    mov ah, 0eh
-    mov al, 'A' ;Character to be output
-    mov bx, 0 ; Page number
-    int 0x10 ; Calls the BIOS, tells it to output al register;
+    mov si, message ;does some shit
+    call print ;I am honored to print text to the screen. Such a significant day in programming history, the day when I, Dexter Harrison, printed some text. This
     jmp $
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+print_char:
+    mov ah, 0eh
+    int 0x10 ; Calls the BIOS, tells it to output al register;
+    ret
+message: db "EngynOS Bootloader test!", 0
 times 510-  ($ - $$) db 0  ;What the fuck???
 dw 0xAA55 ; this is important i think
